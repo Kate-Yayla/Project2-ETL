0eae9d959ed8930a2ea533568b7d4b98ea0d9110
Felicia <84341676+Felicia620@users.noreply.github.com>
2021-08-26 20:10:15 -0400

    table sql file with final edits

--- youtube_trends_table.sql
+++ youtube_trends_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE combined(
+	combined_id INT,
 	video_id VARCHAR(100) NOT NULL,
-	trending_date DATE,
-	video_title VARCHAR(100),
-	channel_title VARCHAR(100),
+	trending_date VARCHAR,
+	video_title VARCHAR,
+	channel_title VARCHAR,
 	category_id INT NOT NULL,
-	tags VARCHAR(100),
+	tags VARCHAR,
 	views INT,
 	likes INT,
 	dislikes INT,
@@ -12,152 +13,9 @@ CREATE TABLE combined(
 	comments_disabled BOOLEAN,
 	ratings_disabled BOOLEAN,
 	video_error_or_removed BOOLEAN,
-	kind VARCHAR(100),
-	etag VARCHAR(100),
-	items VARCHAR(100),
-	category_title VARCHAR(100),
+	kind VARCHAR,
+	etag VARCHAR,
+	items VARCHAR,
+	category_title VARCHAR,
 	country VARCHAR(100)
 );
-
-SELECT category_title, country, COUNT(views) AS "Views Count per Category by Country"
-FROM combined
-GROUP BY category_title, country
-ORDER BY country, "Views Count per Category by Country" DESC
-
-SELECT category_title, country, likes
-FROM combined
-GROUP BY category_title, country, likes
-ORDER BY country, "Likes Count per Category by Country" DESC
-
-SELECT category_title, country, likes
-FROM combined
-GROUP BY category_title, country, likes
-ORDER BY country DESC
-
-SELECT category_title, country, COUNT(views) AS "Views Count per Category by Country"
-FROM combined
-GROUP BY category_title, country
-ORDER BY country, "Views Count per Category by Country" DESC
-
-SELECT category_title, country, SUM(likes)
-FROM combined
-GROUP BY category_title, country 
-ORDER BY country, SUM(likes) DESC
-
-SELECT category_title, country, SUM(dislikes)
-FROM combined
-GROUP BY category_title, country 
-ORDER BY country, SUM(dislikes) ASC
-
-SELECT channel_title, country, SUM(views)
-FROM combined
-GROUP BY channel_title, country 
-ORDER BY country, SUM(views) DESC
-
-SELECT *
-FROM
-	(SELECT usa.country, usa.category_title, SUM(usa.likes)
-	 FROM combined usa
-	 WHERE usa.country='US'
-GROUP BY usa.country, usa.category_title
-ORDER BY SUM(usa.likes) DESC
-FETCH FIRST 5 ROWS ONLY) usa
-UNION ALL
-SELECT *
-FROM
-	(SELECT mx.country, mx.category_title, SUM(mx.likes)
-	 FROM combined mx
-	 WHERE mx.country='MX'
-GROUP BY mx.country, mx.category_title
-ORDER BY SUM(mx.likes) DESC
-FETCH FIRST 5 ROWS ONLY) mx
-UNION ALL
-SELECT *
-FROM
-	(SELECT ca.country, ca.category_title, SUM(ca.likes)
-	 FROM combined ca
-	 WHERE ca.country='CA'
-GROUP BY ca.country, ca.category_title
-ORDER BY SUM(ca.likes) DESC
-FETCH FIRST 5 ROWS ONLY) ca
-
-SELECT *
-FROM
-	(SELECT usa.country, usa.channel_title, SUM(usa.likes)
-	 FROM combined usa
-	 WHERE usa.country='US'
-GROUP BY usa.country, usa.channel_title
-ORDER BY SUM(usa.likes) DESC
-FETCH FIRST 5 ROWS ONLY) usa
-UNION ALL
-SELECT *
-FROM
-	(SELECT mx.country, mx.channel_title, SUM(mx.likes)
-	 FROM combined mx
-	 WHERE mx.country='MX'
-GROUP BY mx.country, mx.channel_title
-ORDER BY SUM(mx.likes) DESC
-FETCH FIRST 5 ROWS ONLY) mx
-UNION ALL
-SELECT *
-FROM
-	(SELECT ca.country, ca.channel_title, SUM(ca.likes)
-	 FROM combined ca
-	 WHERE ca.country='CA'
-GROUP BY ca.country, ca.channel_title
-ORDER BY SUM(ca.likes) DESC
-FETCH FIRST 5 ROWS ONLY) ca
-
-SELECT *
-FROM
-	(SELECT usa.country, usa.channel_title, SUM(usa.dislikes)
-	 FROM combined usa
-	 WHERE usa.country='US'
-GROUP BY usa.country, usa.channel_title
-ORDER BY SUM(usa.dislikes) DESC
-FETCH FIRST 3 ROWS ONLY) usa
-UNION ALL
-SELECT *
-FROM
-	(SELECT mx.country, mx.channel_title, SUM(mx.dislikes)
-	 FROM combined mx
-	 WHERE mx.country='MX'
-GROUP BY mx.country, mx.channel_title
-ORDER BY SUM(mx.dislikes) DESC
-FETCH FIRST 3 ROWS ONLY) mx
-UNION ALL
-SELECT *
-FROM
-	(SELECT ca.country, ca.channel_title, SUM(ca.dislikes)
-	 FROM combined ca
-	 WHERE ca.country='CA'
-GROUP BY ca.country, ca.channel_title
-ORDER BY SUM(ca.dislikes) DESC
-FETCH FIRST 3 ROWS ONLY) ca
-
-SELECT *
-FROM
-	(SELECT usa.country, usa.channel_title, SUM(usa.views)
-	 FROM combined usa
-	 WHERE usa.country='US'
-GROUP BY usa.country, usa.channel_title
-ORDER BY SUM(usa.views) ASC
-FETCH FIRST 3 ROWS ONLY) usa
-UNION ALL
-SELECT *
-FROM
-	(SELECT mx.country, mx.channel_title, SUM(mx.views)
-	 FROM combined mx
-	 WHERE mx.country='MX'
-GROUP BY mx.country, mx.channel_title
-ORDER BY SUM(mx.views) ASC
-FETCH FIRST 3 ROWS ONLY) mx
-UNION ALL
-SELECT *
-FROM
-	(SELECT ca.country, ca.channel_title, SUM(ca.views)
-	 FROM combined ca
-	 WHERE ca.country='CA'
-GROUP BY ca.country, ca.channel_title
-ORDER BY SUM(ca.views) ASC
-FETCH FIRST 3 ROWS ONLY) ca
